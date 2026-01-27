a36b0066a1b443834957fd568b57f24792ecc050
Meeka Simson <meekasimson.26csb@licet.ac.in>
2026-01-27 20:45:30 +0530

    Time: 2 ms (99.13%), Space: 47.2 MB (47.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map <Integer,Integer> map = new HashMap<>();
+        HashMap <Integer,Integer> map = new HashMap<>();
+        int compliment;
         for(int i=0;i<nums.length;i++){
-            int diff = target - nums[i];
+            compliment = target -nums[i];
 
-            if(map.containsKey(diff)){
-                return new int[]{map.get(diff),i};
+            if(map.containsKey(compliment)){
+                return new int[] { map.get(compliment),i};
             }
             map.put(nums[i],i);
         }
-        
-        return new int []{};
-        
-}
+
+        return new int[0];
+    }
 }
