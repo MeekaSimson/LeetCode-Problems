42cfd735172a866604e4c8c0d3c0e4eb848c2de0
Meeka Simson <meekasimson.26csb@licet.ac.in>
2026-01-28 03:19:02 +0530

    Time: 15 ms (24.67%), Space: 46.6 MB (20.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-    if(s.length() != t.length()) return false;
-        int arr[] = new int[256];
-        for(int i=0;i<s.length();i++){
-          arr[s.charAt(i)]++;
-          arr[t.charAt(i)]--;  
-        }
-        for( int i=0;i<256;i++){
-            if(arr[i]!=0) return false;
-        }
+    HashMap <Character,Integer> map = new HashMap<>();
 
-        return true;
+    for(char c:s.toCharArray()){
+        map.put(c,map.getOrDefault(c,0)+1);
     }
-}
+    for(char c:t.toCharArray()){
+        if(!map.containsKey(c)){
+            return false;
+        }
+        map.put(c,map.get(c)-1);
+
+        if(map.get(c)==0){
+            map.remove(c);
+        }
+    }
+    return map.isEmpty();
+
+}}
