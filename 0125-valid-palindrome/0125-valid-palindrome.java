ac977899a1029a71e4b4972640a31b0f43caf78d
Meeka Simson <meekasimson.26csb@licet.ac.in>
2026-02-10 21:15:04 +0530

    Time: 3 ms (62.94%), Space: 44.4 MB (66.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,18 +4,10 @@ class Solution {
         int right = s.length()-1;
 
         while(left<right){
-            if(left<right && !Character.isLetterOrDigit(s.charAt(left))){ 
-                left++;
-                continue;
-                }
-            if(left<right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-                continue;
-                }
-            if(Character.toLowerCase(s.charAt(left))
-            != Character.toLowerCase(s.charAt(right))){
-                return false;
-            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))) return false;
             left++;
             right--;
         }
